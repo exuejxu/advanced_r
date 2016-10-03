548f4d43bfd8f8f3657dc3b79c54a7b3195e4285
Xuejun Xu <xuejun.xu@ericsson.com>
2016-10-03 16:12:39 +0200

    add method 2 for brute force

--- rlab6.R
+++ rlab6.R
@@ -7,6 +7,7 @@ knapsack_objects <-
   )
 
 # 1.1.2 Brute force search
+# wiki method
 brute_force_knapsack <- function(x, W){
   n = nrow(x)
   
@@ -52,6 +53,35 @@ brute_force_knapsack <- function(x, W){
   
 }
 
+# lab specified method
+brute_force_knapsack1 <- function(x, W){
+  n = nrow(x)
+  maxv = 0
+  ev = c()
+  
+  for(i in 1:(2^n)){
+    iv = intToBits(i)
+    sv = 0
+    sw = 0
+    tv = c()
+    for(j in 1:n){
+      if(iv[j] == 1){
+        sv = sv + x[j, ]$v
+        sw = sw + x[j, ]$w
+        tv = c(tv, j)
+      }
+    }
+
+    if((sv > maxv) && (sw <= W)){
+      maxv = sv
+      ev = tv
+    }
+  }
+  
+  list(value=round(maxv), elements=ev)
+}
+
 
 brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
+brute_force_knapsack1(x = knapsack_objects[1:12,], W = 3500)
 
