4b502230bf471dc9f0a75dae9def92f6a0f7fdbe
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-22 14:54:42 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,47 +1,34 @@
-# Use knearest()
-fit7 = knearest(traindata, k=5, testdata)
-true7 = testdata[,ncol(data)]
+#### Assignment 2. Inference about lifetime of machines
+# 2.1 Import the data to R.
+df <- read.csv("C:/Users/Sam/Desktop/machine learning/lab1/machines.csv")
 
-sens7 = c()
-spc7 = c()
-pi = seq(from = 0.05, to = 0.95, by = 0.05)
-for (i in 1:length(pi)){
-  prediction7 = as.integer(fit7>pi[i])
-  confusion7 = table(true7, prediction7)
-  sens7[i] = confusion7[2,2]/sum(confusion7[2,])
-  spc7[i] = confusion7[1,1]/sum(confusion7[1,])
+#### 2.2
+# computes the log-likelihood for a given theta & a given data vector x.
+n = nrow(df)
+
+# log-likelihood fun
+log_likelihood <- function(n, theta, x){
+  log_likelihood <- n*log(theta) - theta * sum(x)
+  return(log_likelihood)
 }
-sens7
-spc7
 
-# use kknn()
-library(kknn)
-fit8 = kknn(as.factor(train$Spam)~.,train,test, k = 5)
+# loop in a given theta
+theta <- seq(from = 0.1, to = 5, by = 0.1)
+N = length(theta)
 
-prob8 = fit8$prob # probability %
-true8 = testdata[, ncol(data)]
-
-sens8 = c()
-spc8 = c()
-pi = seq(from = 0.05, to = 0.95, by = 0.05)
-for (i in 1:length(pi)){
-  prediction8 = as.integer(prob8[,2]>pi[i])  # classify(0,1)
-  confusion8 = table(true8, prediction8)
-  sens8[i] = confusion8[2,2]/sum(confusion8[2,])
-  spc8[i] = confusion8[1,1]/sum(confusion8[1,])
+ll <- c()
+for(i in 1:N){
+  ll[i] = log_likelihood(n=n, theta = theta[i], x = df$Length)
 }
-sens8
-spc8
+ll
 
-# plot the corresponding ROC curves of 2 methods in 1 plot
-x1  <- 1 - spc7 # FPR
-y1 <- sens7     # TPR
-x2 <- 1 - spc8
-y2 <- sens8
+# Plot the curve showing the dependence of log-likelihood on theta
+plot(x=theta,y=ll,col = "red", type = "l",
+     xlab = "theta", ylab = "log-likelihood", 
+     xlim = c(0.1,5), ylim = c(-150, -20))
 
-# 1st plot
-plot(x=x1, y=y1,col = "red", type = "l",  # type"l"for line
-     xlab = "fpr", ylab = "tpr", 
-     xlim = c(0, 1), ylim = c(0, 1))  #usually x,ylim=c(0,1) for probility
-# 2nd plot
-lines(x=x2, y=y2,col = "blue", type = "l")
+# value of theta on maximum likelihood
+index_max_y1 <- which.max(ll)
+theta_optimal1 <- theta[index_max_y1]
+theta_optimal1
+
