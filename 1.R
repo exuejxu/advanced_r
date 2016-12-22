ed626ad6bdd540b5efd7dfc6bbef9e5eee94972a
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-22 20:17:32 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,35 +1,56 @@
-#### 2.4 log-likelihood fun for Bayesian model with a prior
-theta <- seq(from = 0.1, to = 5, by=0.1)
-n = nrow(df)
-N = length(theta)
-lamda = 10
+data = read.csv2("C:/Users/Sam/Desktop/machine learning/lab1 block2/cube.csv")
 
-l_theta <- function(n, theta, x, lamda){
-  l_theta <- (n*log(theta) - theta*sum(x) + log(lamda*exp(-lamda*theta)))
-  return(l_theta)
+### 1.1 Basis expansion function
+myspline = function(x, y, knots){
+  
+  m=length(knots)
+  n=length(x)
+  H=matrix(0,nrow=n,ncol=m+2) #new features
+  H[,1]=1
+  H[,2]=x
+  
+  # loop in knots and ncol
+  for(i in 1:m){
+    # loop in nrow
+    for(j in 1:n){
+      #index_logical = ((knots[i]<=x&x<knots[i+1]))
+      H[j,(i+2)] <- x[j] - knots[i]
+      # from the (i+2)th col to last col, negative elements should be 0  
+      if(H[j, (i+2)] < 0) H[j, (i+2)] = 0
+    }
+  }
+  H
+  
+  # use matrix for linear regression fun()
+  testx = H
+  trainx = H
+  testy = y
+  trainy = y
+  
+  # Estimate w for linear regression
+  w_hat = solve(t(trainx) %*% trainx) %*% t(trainx) %*% trainy
+  
+  # Prediction
+  y_hat = testx %*% w_hat
+  
+  # plot predicted and the original data
+  plot(x,y, col="blue")
+  points(x,y_hat, col = "red", lwd=2)
+  
 }
 
-# loop in a given theta
-ll3 <- c()
-for(i in 1:N){
-  ll3[i] = l_theta(n=n, theta = theta[i], x = df$Length, lamda)
-}
-ll3
+### 1.2 Test with knots at 3 and 6
+x = data$x
+y = data$y
+knots = c(2,4)
+myspline(x, y, knots)
 
-# Plot the curve showing the dependence of l(theta)
-plot(x= theta, y=ll3,col = "green", type = "l", xlab = "theta", 
-     ylab = "l(theta)", xlim = c(0.1,5), ylim = c(-150, -20))
+### 1.3 Use smooth.spline() to fit the same data
+res1=smooth.spline(data$x,data$y,df=10)
 
-# value of theta on maximum likelihood
-index_max_y3 <- which.max(ll3)
-theta_optimal3 <- theta[index_max_y3]
-theta_optimal3
+# y_hat
+pred = predict(res1,x=data$x)$y
 
-#### 2.5 use theta value found in step 2 to generate 50 new observations
-# use standard random number generators,random exponential distribution
-random = rexp(50, rate = theta_optimal1)
-
-# Create the histograms of the original and the new data
-hist(random, xlab = "Lifetime", main = "Histogram of new data")
-hist(df$Length, xlab = "Lifetime", main = "Histogram of original data")
-
+# plot predicted and the original data
+plot(x,y, col="blue")
+lines(res1, col = "green", lwd=2)
