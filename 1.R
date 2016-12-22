d72a11ffe750ed61b958eb3bdc0f2be74f523740
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-22 17:10:53 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,17 +1,35 @@
-ll2 <- c()
-for(i in 1:N){
-  ll2[i] = log_likelihood(n=6, theta = theta[i], x = df$Length[1:6])
+#### 2.4 log-likelihood fun for Bayesian model with a prior
+theta <- seq(from = 0.1, to = 5, by=0.1)
+n = nrow(df)
+N = length(theta)
+lamda = 10
+
+l_theta <- function(n, theta, x, lamda){
+  l_theta <- (n*log(theta) - theta*sum(x) + log(lamda*exp(-lamda*theta)))
+  return(l_theta)
 }
-ll2
+
+# loop in a given theta
+ll3 <- c()
+for(i in 1:N){
+  ll3[i] = l_theta(n=n, theta = theta[i], x = df$Length, lamda)
+}
+ll3
+
+# Plot the curve showing the dependence of l(theta)
+plot(x= theta, y=ll3,col = "green", type = "l", xlab = "theta", 
+     ylab = "l(theta)", xlim = c(0.1,5), ylim = c(-150, -20))
 
 # value of theta on maximum likelihood
-index_max_y2 <- which.max(ll2)
-theta_optimal2 <- theta[index_max_y2]
-theta_optimal2
+index_max_y3 <- which.max(ll3)
+theta_optimal3 <- theta[index_max_y3]
+theta_optimal3
 
-# Plot the curve showing the dependence of log-likelihood on theta
-# put the two log-likelihood curves (from step 2 and 3) in the same plot
-plot(x=theta,y=ll,col = "red", type = "l",
-     xlab = "theta", ylab = "log-likelihood", 
-     xlim = c(0.1,5), ylim = c(-150, 0))
-lines(x=theta,y=ll2,col = "blue")
+#### 2.5 use theta value found in step 2 to generate 50 new observations
+# use standard random number generators,random exponential distribution
+random = rexp(50, rate = theta_optimal1)
+
+# Create the histograms of the original and the new data
+hist(random, xlab = "Lifetime", main = "Histogram of new data")
+hist(df$Length, xlab = "Lifetime", main = "Histogram of original data")
+
