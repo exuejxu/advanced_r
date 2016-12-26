3de6ddce9f8b0ea4a6d18b418d14e0f5bb3b0be2
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-26 21:34:58 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,53 +1,53 @@
-###### Assignment 1. High-dimensional methods
-data0 = read.csv2("C:/Users/Sam/Desktop/machine learning/lab3 block2/data.csv")
-
-
-### 1.1. Divide data into training and test sets (70/30) without scaling.
-n=dim(data0)[1]
-set.seed(12345)
-id=sample(1:n, floor(n*0.7)) 
-train=data0[id,]
-test=data0[-id,]
-
-# Perform nearest shrunken centroid classification of training data
-data=as.data.frame(train) #with scaling: data=as.data.frame(scale(data)) 
-data$Conference=as.factor(train$Conference) 
-
-library(pamr)
-rownames(data)=1:nrow(data)
-# predictors
-x=as.matrix(t(data[,-4703])) 
-# target
-y=data[[4703]] #data$Conference
-
-mydata=list(x=x,y=as.factor(y),geneid=as.character(1:nrow(x)), genenames=rownames(x))
-model=pamr.train(mydata,threshold=seq(0,4, 0.1))
-
-# threshold is chosen by cross-validation.
-cvmodel=pamr.cv(model,mydata)
-# find value of threshold with min error 1.4
-print(cvmodel) 
-# centroid plot
-pamr.plotcv(cvmodel) 
-
-# centroid plot by the optimal threshold
-pamr.plotcen(model, mydata, threshold=1.3)  
-pamr.plotcen(model, mydata, threshold=1.4) 
-
-# get features' name list
-a=pamr.listgenes(model,mydata,threshold=1.4) 
-cat(paste(colnames(data)[as.numeric(a[,1])], collapse='\n' ) )
-
-# test error for test dataset
-test$Conference=as.factor(test$Conference) 
-rownames(test)=1:nrow(test)
-# predictors
-xt=as.matrix(t(test[,-4703])) 
-# target
-yt=test[[4703]] #data$Conference
-
-pred_test <- pamr.predict(model, newx = xt, threshold = 1.4, type = "class")
-confusion_test <- table("true" = yt, "predicted" = pred_test)
-test_error <- 1 - sum(diag(confusion_test))/sum(confusion_test)
-test_error
 # 0.1
+
+
+
+### 1.2. Compute the test error and the number of the contributing features
+### a.	Elastic net
+# with the binomial response: family
+# penalty is selected by the cross-validation #####
+
+library(glmnet)
+# For either "binomial" or "multinomial", y will be coerced into a factor.
+# train data
+# predictors
+x2=as.matrix(train[,-4703])
+# target
+y2=train[[4703]]
+
+# test data
+# predictors
+x3=as.matrix(test[,-4703])
+# target
+y3=test[[4703]]
+
+cv.fit = cv.glmnet(x=x2, y=as.factor(y2), family="binomial", alpha = 0.5) ### alpha ###
+pred = predict.cv.glmnet(cv.fit, newx=x3, type = "class") #class: get 0 and 1
+pred = as.numeric(pred)
+
+tab = table(true=y3, pred=pred)
+tab
+# compute misclassification rates/ test error
+mis = 1-sum(diag(tab))/sum(tab)
+mis
+#[1] 0.2
+
+
+# number of contributing features ##########
+features <- cv.fit$nzero[which(cv.fit$lambda == cv.fit$lambda.min)]
+#s31 
+#33 
+
+
+### b.	Support vector machine with vanilladot kernel.
+library(kernlab)
+
+fit1 = ksvm(Conference~., data = train, type = "C-svc", kernel="vanilladot")  ### Warning but ok!
+Y1 = predict(fit1, test)
+
+tab1 = table(true=test$Conference, pred=Y1)
+tab1
+# compute misclassification rates
+mis1 = 1-sum(diag(tab1))/sum(tab1)
+mis1
+#[1] 0.05
