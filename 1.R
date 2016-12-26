a6ebce583ccb598dcf17bff3c21e9087f085c90a
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-26 21:43:33 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,53 +1,32 @@
-# 0.1
-
-
-
-### 1.2. Compute the test error and the number of the contributing features
-### a.	Elastic net
-# with the binomial response: family
-# penalty is selected by the cross-validation #####
-
-library(glmnet)
-# For either "binomial" or "multinomial", y will be coerced into a factor.
-# train data
-# predictors
-x2=as.matrix(train[,-4703])
-# target
-y2=train[[4703]]
-
-# test data
-# predictors
-x3=as.matrix(test[,-4703])
-# target
-y3=test[[4703]]
-
-cv.fit = cv.glmnet(x=x2, y=as.factor(y2), family="binomial", alpha = 0.5) ### alpha ###
-pred = predict.cv.glmnet(cv.fit, newx=x3, type = "class") #class: get 0 and 1
-pred = as.numeric(pred)
-
-tab = table(true=y3, pred=pred)
-tab
-# compute misclassification rates/ test error
-mis = 1-sum(diag(tab))/sum(tab)
-mis
-#[1] 0.2
-
-
-# number of contributing features ##########
-features <- cv.fit$nzero[which(cv.fit$lambda == cv.fit$lambda.min)]
-#s31 
-#33 
-
-
-### b.	Support vector machine with vanilladot kernel.
-library(kernlab)
-
-fit1 = ksvm(Conference~., data = train, type = "C-svc", kernel="vanilladot")  ### Warning but ok!
-Y1 = predict(fit1, test)
-
-tab1 = table(true=test$Conference, pred=Y1)
-tab1
-# compute misclassification rates
-mis1 = 1-sum(diag(tab1))/sum(tab1)
-mis1
 #[1] 0.05
+
+
+### 1.3. Implement Benjamini-Hochberg method for the original data
+
+# computing p-values
+features_no = ncol(data0)-1 #4702
+p = c()
+for(f in 1:features_no){
+  res = t.test(data0[,f]~Conference, data=data0, alternative="two.sided")
+  p[f] = res$p.value
+}
+p #4702
+order = order(p)
+p = p[order]
+
+# Define
+alpha = 0.05
+index = c()
+for(j in 1:length(p)){
+  if(p[j] < alpha*j/length(p)) index = c(index, j)
+}
+index
+
+# reject all hypotheses H0j for which p[j] <= p[L]
+L = max(index)
+L
+#[1] 39
+
+names(data0)[order][1:39]
+#[1] "papers"        "submission"    "position"      "published"     "important"     "call"          "conference"    "candidates"    "dates"         "paper"         "topics"        "limited"       "candidate"     "camera"        "ready"         "authors"       "phd"           "projects"      "org"           "chairs"        "due"           "original"      "notification"  "salary"        "record"        "skills"        "held"          "team"          "pages"         "workshop"      "committee"    
+#[32] "proceedings"   "apply"  
