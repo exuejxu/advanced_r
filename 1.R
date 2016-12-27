7f8d0c798313debb3d964f7a096587bdb9012e70
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-27 22:47:14 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -33,3 +33,37 @@ plot(U[,1], main="Traceplot, PC1")
 plot(U[,2],main="Traceplot, PC2")
 
 
+### 2.3 ICA
+# Perform Independent Component Analysis of PC1 & PC2, n.comp=2
+set.seed(12345) 
+library(fastICA)
+a = fastICA(data1, n.comp=2,alg.typ = "parallel", fun = "logcosh", alpha = 1, 
+            method = "R", row.norm = FALSE, maxit = 200, tol = 0.0001, verbose = TRUE)
+# Report matrix W
+a$W
+
+# Explain the roll of the matrix w'= k.w
+Wp = a$K%*%a$W
+Wp
+
+# present its columns in form of the trace plots.
+plot(Wp[,1], main = "Traceplot, PC1")
+plot(Wp[,2], main = "Traceplot, PC2")
+
+# Make the score plot of the first two latent features
+plot(a$S[,1], a$S[,2])
+
+
+### 2.4 Fit a PCR model to the training data 
+# where number of components is selected by cross validation.
+library(pls)
+set.seed(12345)
+
+# where number of components is selected by cross validation.
+pcr.fit = pcr(Viscosity~., data = data, scale = FALSE, validation = "CV")
+summary(pcr.fit)
+
+# Plot MSPE, the dependence of the mean-square predicted error
+validationplot(pcr.fit, val.type = "MSEP")
+
+
