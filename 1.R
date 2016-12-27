fd2135f4477ae7ff5c879b2ad5b601dcd331c0cf
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-27 22:21:15 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,111 +1,35 @@
-### 1.3 Compute and plot the 95%(default) confidence bands for the regression tree model from step 2
-# non-parametric bootstrap.
-library(boot)
-#reordering data according to feature:MET
-data2=mydata[order(mydata$MET),]
+####### Assignment 2. Principal components
+data <- read.csv2("C:/Users/Sam/Desktop/machine learning/lab4/NIRSpectra.csv")
 
-# computing bootstrap samples
-f=function(data, ind){
-  # extract bootstrap sample
-  data1=data[ind,]
-  # fit regression tree model
-  fit = tree(EX~MET, data = data1, control = tree.control(nobs = 48, minsize = 8))
-  res = prune.tree(fit, best = bestsize)  
-  #predict values for all Area values from the original data
-  EXP=predict(res,newdata=data2) 
-  return(EXP)
-}
+### 2.1 Conduct a standard PCA
+# by using the feature space
+# plot to explain how much variation is explained by each feature.
 
-# make bootstrap
-res=boot(data2, f, R=2000) 
+data1=data
+data1$Viscosity=c()
+res=prcomp(data1)
+lambda=res$sdev^2
+#eigenvalues
+lambda
+#proportion of variation
+# Select the minimal number of components 
+# explaining at least 99% of the total variance.
+sprintf("%2.3f",lambda/sum(lambda)*100)
+screeplot(res)
 
-# Bootstrap cofidence bands for linear model
-# compute confidence bands
-e=envelope(res)
-
-# compute fitted line
-fit2=tree(EX~MET, data=data2, control = tree.control(nobs = 48, minsize = 8)) 
-fianlTree = prune.tree(fit2, best = bestsize) #######
-EXP=predict(fianlTree)
-
-#plot fitted line
-plot(mydata$MET, mydata$EX, pch=21, bg="orange", xlab = "MET", ylab = "EX")
-points(data2$MET,EXP,type="l") 
-
-# plot cofidence bands
-points(data2$MET,e$point[2,], type="l", col="blue")
-points(data2$MET,e$point[1,], type="l", col="blue")
+# Select the minimal number of components 
+# explaining at least 99% of the total variance.
+# scores plot in the coordinates (PC1, PC2)
+plot(res$x[,1], res$x[,2])
 
 
-### 1.4 Compute and plot the 95% confidence bands && prediction bands
-# by using parametric bootstrap.
-# assume Y~N(mu, s^2)
-library(boot)
-#reordering data according to feature:MET
-data2=mydata[order(mydata$MET),]
-fit2 = tree(EX~MET, data = data2, control = tree.control(nobs = 48, minsize = 8))
-mle = prune.tree(fit2, best = bestsize) #####
+### 2.2 Trace plots for the loadings of PC1 & PC2.
 
-rng=function(data, mle) {
-  data1=data.frame(EX=mydata$EX, MET=mydata$MET)
-  n=length(mydata$EX)
-  #generate new target:EX
-  data1$EX=rnorm(n,predict(mle, newdata=data1),sd(residuals(mle)))
-  return(data1)
-}
+#U = loadings(res)
+U=res$rotation
+head(U)
 
-f1=function(data1){
-  #fit regression tree model
-  res=tree(EX~MET, data= data1, control = tree.control(nobs = 48, minsize = 8))
-  res = prune.tree(res, best = bestsize) ######
-  #predict values for all Area values from the original data
-  EXP=predict(res,newdata=data2) 
-  return(EXP)
-}
-# make bootstrap
-res2=boot(data2, statistic=f1, R=1000, mle=mle, ran.gen=rng, sim="parametric")
-
-# Bootstrap cofidence bands for tree model
-# compute confidence bands
-e2=envelope(res2)
-
-# compute fitted line
-fit2=tree(EX~MET, data=data2, control = tree.control(nobs = 48, minsize = 8)) 
-fianlTree = prune.tree(fit2, best = bestsize) #######
-EXP=predict(fianlTree)
-
-# plot fitted line
-plot(mydata$MET, mydata$EX, pch=21, bg="orange", xlab = "MET", ylab = "EX",
-     ylim = c(100,450), xlim = c(0,90))
-points(data2$MET,EXP,type="l") 
-
-# plot cofidence bands
-points(data2$MET,e2$point[2,], type="l", col="blue")
-points(data2$MET,e2$point[1,], type="l", col="blue")
-
-# compute Prediction bands
-data2=mydata[order(mydata$MET),]
-fit2 = tree(EX~MET, data = data2, control = tree.control(nobs = 48, minsize = 8))
-mle = prune.tree(fit2, best = bestsize)
-
-f2 = function(data1){
-  #fit model
-  temp = tree(EX~MET, data = data1, control = tree.control(nobs = 48, minsize = 8))
-  res = prune.tree(temp, best = bestsize) #####
-  #predict values for all Area values from the original data
-  EXP = predict(res, newdata = data2)
-  n = length(data2$EX)
-  predictedP = rnorm(n, EXP, sd(residuals(mle)))
-  return(predictedP)
-  
-}
-# R=2000, more number
-res3 = boot(data2, statistic = f2, R = 2000, mle = mle, ran.gen = rng, sim = "parametric") 
-
-e3 = envelope(res3)
-
-# Plot prediction bands
-points(data2$MET, e3$point[2,], type = "l", col = "green")
-points(data2$MET, e3$point[1,], type = "l", col = "green")
+plot(U[,1], main="Traceplot, PC1")
+plot(U[,2],main="Traceplot, PC2")
 
 
