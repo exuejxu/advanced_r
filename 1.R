5ffd2a0687981c12a30b6c67c640be56cde15a5e
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-27 19:15:16 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,50 +1,111 @@
-####### Assignment 1. Uncertainty estimation
-# sep = ";", read.csv2
-mydata <- read.csv2("C:/Users/Sam/Desktop/machine learning/lab4/State.csv")
+### 1.3 Compute and plot the 95%(default) confidence bands for the regression tree model from step 2
+# non-parametric bootstrap.
+library(boot)
+#reordering data according to feature:MET
+data2=mydata[order(mydata$MET),]
 
-### 1.1 Reorder data with respect to the increase of MET
-ind <- order(mydata$MET,decreasing = FALSE)
-for(i in 1:ncol(mydata)){
-  mydata[, i] = mydata[, i][ind]
+# computing bootstrap samples
+f=function(data, ind){
+  # extract bootstrap sample
+  data1=data[ind,]
+  # fit regression tree model
+  fit = tree(EX~MET, data = data1, control = tree.control(nobs = 48, minsize = 8))
+  res = prune.tree(fit, best = bestsize)  
+  #predict values for all Area values from the original data
+  EXP=predict(res,newdata=data2) 
+  return(EXP)
 }
-mydata
 
-# Plot MET versus EX
-plot(mydata$MET, mydata$EX, col = "blue", xlab = "MET", ylab = "EX")
+# make bootstrap
+res=boot(data2, f, R=2000) 
 
-### 1.2 Fit a regression tree model with target EX~feature MET 
-# the number of the leaves is selected by cross-validation #######
+# Bootstrap cofidence bands for linear model
+# compute confidence bands
+e=envelope(res)
 
-# Selecting optimal tree by penalizing
-# use the entire data set, nrow(mydata)=48
-# set minimum number of observations in a leaf = 8(setting minsize in tree.control)
-library(tree)
-fit = tree(EX~MET, data = mydata, control = tree.control(nobs = 48, minsize = 8))
-predict(fit)
-set.seed(12345)
-cv.res = cv.tree(fit) #cross-validation
-plot(cv.res$size, cv.res$dev, type = "b", col = "red")
-plot((cv.res$k), cv.res$dev, type = "b", col = "red")
-plot(cv.res)
+# compute fitted line
+fit2=tree(EX~MET, data=data2, control = tree.control(nobs = 48, minsize = 8)) 
+fianlTree = prune.tree(fit2, best = bestsize) #######
+EXP=predict(fianlTree)
 
-# Report optimal tree
-# gives the deviance for each K (small is better)
-cv.res$dev
-# [1] 192109.0 186515.5 188981.5
-index <- which.min(cv.res$dev) #5
-bestsize <- cv.res$size[index]
-bestsize  # 3
+#plot fitted line
+plot(mydata$MET, mydata$EX, pch=21, bg="orange", xlab = "MET", ylab = "EX")
+points(data2$MET,EXP,type="l") 
 
-# refit the model with the number of leafs of best size
-finalTree = prune.tree(fit, best = bestsize) ##########choose from above plot, min deviance
-Ffit = predict(finalTree)
-plot(finalTree)
-text (finalTree , pretty =0)
+# plot cofidence bands
+points(data2$MET,e$point[2,], type="l", col="blue")
+points(data2$MET,e$point[1,], type="l", col="blue")
 
-# Plot the original and the fitted data
-plot(mydata$MET, mydata$EX, col = "blue", xlab = "MET", ylab = "EX")
-points(mydata$MET, Ffit, col = "red")
 
-# Plot histogram of residuals.  
-residuals = residuals(finalTree)
-hist(residuals)
+### 1.4 Compute and plot the 95% confidence bands && prediction bands
+# by using parametric bootstrap.
+# assume Y~N(mu, s^2)
+library(boot)
+#reordering data according to feature:MET
+data2=mydata[order(mydata$MET),]
+fit2 = tree(EX~MET, data = data2, control = tree.control(nobs = 48, minsize = 8))
+mle = prune.tree(fit2, best = bestsize) #####
+
+rng=function(data, mle) {
+  data1=data.frame(EX=mydata$EX, MET=mydata$MET)
+  n=length(mydata$EX)
+  #generate new target:EX
+  data1$EX=rnorm(n,predict(mle, newdata=data1),sd(residuals(mle)))
+  return(data1)
+}
+
+f1=function(data1){
+  #fit regression tree model
+  res=tree(EX~MET, data= data1, control = tree.control(nobs = 48, minsize = 8))
+  res = prune.tree(res, best = bestsize) ######
+  #predict values for all Area values from the original data
+  EXP=predict(res,newdata=data2) 
+  return(EXP)
+}
+# make bootstrap
+res2=boot(data2, statistic=f1, R=1000, mle=mle, ran.gen=rng, sim="parametric")
+
+# Bootstrap cofidence bands for tree model
+# compute confidence bands
+e2=envelope(res2)
+
+# compute fitted line
+fit2=tree(EX~MET, data=data2, control = tree.control(nobs = 48, minsize = 8)) 
+fianlTree = prune.tree(fit2, best = bestsize) #######
+EXP=predict(fianlTree)
+
+# plot fitted line
+plot(mydata$MET, mydata$EX, pch=21, bg="orange", xlab = "MET", ylab = "EX",
+     ylim = c(100,450), xlim = c(0,90))
+points(data2$MET,EXP,type="l") 
+
+# plot cofidence bands
+points(data2$MET,e2$point[2,], type="l", col="blue")
+points(data2$MET,e2$point[1,], type="l", col="blue")
+
+# compute Prediction bands
+data2=mydata[order(mydata$MET),]
+fit2 = tree(EX~MET, data = data2, control = tree.control(nobs = 48, minsize = 8))
+mle = prune.tree(fit2, best = bestsize)
+
+f2 = function(data1){
+  #fit model
+  temp = tree(EX~MET, data = data1, control = tree.control(nobs = 48, minsize = 8))
+  res = prune.tree(temp, best = bestsize) #####
+  #predict values for all Area values from the original data
+  EXP = predict(res, newdata = data2)
+  n = length(data2$EX)
+  predictedP = rnorm(n, EXP, sd(residuals(mle)))
+  return(predictedP)
+  
+}
+# R=2000, more number
+res3 = boot(data2, statistic = f2, R = 2000, mle = mle, ran.gen = rng, sim = "parametric") 
+
+e3 = envelope(res3)
+
+# Plot prediction bands
+points(data2$MET, e3$point[2,], type = "l", col = "green")
+points(data2$MET, e3$point[1,], type = "l", col = "green")
+
+
