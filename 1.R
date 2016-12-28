11f70d12f04b96a99b96678e3161c9804e8e18ec
Yumeng Li <lemonisa.lee@gmail.com>
2016-12-28 20:48:57 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,40 +1,127 @@
+##############temperature forecast for a date and place in Sweden
+# predicted temperatures from 4 am to 24 pm in an interval of 2 hours.
+# Use a kernel that is the sum of three Gaussian kernels
+
+set.seed(1234567890)
+library(geosphere)
+
+stations <- read.csv("C:/Users/Sam/Desktop/machine learning/lab5/stations.csv")
+temps <- read.csv("C:/Users/Sam/Desktop/machine learning/lab5/temps50k.csv")
+
+st1 <- merge(stations,temps,by="station_number")
+ind <- sample(1:50000, 5000)
+st <- st1[ind,]
+
+# define three values
+h_distance <- 5
+h_date <- 5
+h_time <- 5
+
+# The point to predict (up to the students)
+a <- 58.4274 
+b <- 14.826
+# The date to predict (up to the students)
+#date_p <- "2013-11-04" 
+#times_p <- c("04:00:00", "06:00:00", ..., "24:00:00")  # 11 elements
+
+# number of prediction
+n = (24 - 4)/2 + 1  #11
+
+# fliter the data posterior to the day and hour of forecast.
+# row 16462 is the position of "2013-11-04" 
+datep = as.numeric(st1$date[16462])
+
+stnew = st[as.numeric(st$date) < datep, ]
 
 
-#####################################
-#####################################
-# Estimate MSE of the optimal model by using the test set. 
-pcr.fit1 = pcr(Viscosity~., ncomp=40,  data = train, scale = FALSE, validation = "none")  # 40######## 30
-summary(pcr.fit1)
+################ distance 
+lat = as.numeric(stnew$latitude) 
+lon = as.vector(stnew$longitude) ### contain NA
+lon = as.numeric(lon)
 
-y1 = predict(pcr.fit1, newdata = test)
-mse = mean((y1-test$Viscosity)^2,na.rm= TRUE)
-mse
+# remove NA from lon
+ind = c()
+for(i in 1:length(lon)){
+  if(is.na(lon[i])) ind = c(ind, i)
+}
+ind 
+
+lon = lon[-ind]
+lat = lat[-ind]
 
 
-### 2.5 Divide the data into training(50%) and test(50%) sets.
-n=dim(data)[1]
-set.seed(12345) 
-id=sample(1:n, floor(n*0.5)) 
-train=data[id,] 
-test=data[-id,]
+# The point to predict (up to the students)
+a <- 58.4274 
+b <- 14.826
 
-#### 2.6 use PLS model
-library(pls)
-set.seed(12345)
+# Gaussian kernels for distance
+h_distance = 5
+dis = numeric(n)
+for(k in 1:length(lat)){
+  dis[k] = distHaversine(c(lat[k], lon[k]), c(a,b))/1000000 #compute and scale the distance 
+  dist_kl = numeric(length(dis))
+  #print(dis)
+  for (j in 1:length(dis)){
+    dist_kl[j] = exp(-(dis[j]/h_distance)^2)  
+  }
+}
+dist_kl      
 
-# Estimate MSE of the optimal model by using the test set.
-pls.fit = plsr(train$Viscosity~., data = train, scale = FALSE, validation = "CV")
-summary(pls.fit)
 
-# Plot MSPE
-validationplot(pls.fit, val.type = "MSEP")
+#################### date
+date = as.numeric(stnew$date)
+# 1 day, no loop in 1:11
+datetest = as.numeric(st1$date[16462])
 
-# Estimate MSE of the optimal model by using the test set. 
-pls.fit2 = plsr(Viscosity~., ncomp=11,  data = train, scale = FALSE, validation = "none")
-summary(pls.fit2)
-y2 = predict(pls.fit2, newdata = test)
-mse2 = mean((y2-test$Viscosity)^2,na.rm= TRUE)
-mse2
+# Gaussian kernels for date
+h_date = 5
+dis = abs(date - datetest)/10000  #compute and scale the distance 
+date_kl = numeric(length(dis))
+for (j in 1:length(dis)){
+  date_kl[j] = exp(-(dis[j]/h_date)^2)  
+}
+date_kl 
 
+
+################## time
+time = as.vector(stnew$time)
+# convert time to numeric
+time = sapply(strsplit(time,":"),
+              function(x) {
+                x <- as.numeric(x)
+                x[1]
+              }
+)
+
+# 11 elements
+timetest = seq(from=4, to=24, by=2)
+
+
+# sum of three Gaussian kernels
+gkernel = function(X, Y, Xtest, h_time){
+  n = length(Xtest)
+  pred = numeric(n)
+  dis = numeric(n)
+  for (i in 1:n){
+    dis = abs(X-Xtest[i])
+    kl = numeric(length(dis))
+    time_kl = numeric(length(dis))
+    for (j in 1: length(dis)){
+      time_kl[j] = exp(-(dis[j]/h_time)^2)
+    }
+    kl = time_kl + dist_kl[i] + date_kl[i]  #sum of three Gaussian kernels
+    #print(kl)
+    pred[i] = sum(kl*Y)/sum(kl)
+  }
+  return(pred)
+}
+
+temperature = as.numeric(stnew$air_temperature) 
+
+# call prediction function
+temp = gkernel(X=time, Y=temperature, Xtest = timetest, h_time = 5) #len:11
+temp
+
+plot(temp, type="o")
 
 
