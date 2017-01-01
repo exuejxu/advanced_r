2f451a00fa93b65fc997fe983458aed640914ce7
Yumeng Li <lemonisa.lee@gmail.com>
2017-01-01 18:46:47 +0100

    Update 1.R

--- 1.R
+++ 1.R
@@ -1,47 +1,91 @@
-##### LAB 6: NEURAL NETWORKS AND DEEP LEARNING
-library(neuralnet)
-set.seed(1234567890)
+#CL2_ASS1
+library(MASS)
 
-# Sample 50 points uniformly at random in the interval [0, 10]
-Var <- runif(50, 0, 10)
-# Apply the sin function to each point
-trva <- data.frame(Var, Sin=sin(Var))
-
-tr <- trva[1:25,] # Training
-va <- trva[26:50,] # Validation
-
-# Random initializaiton of the weights in the interval [-1, 1]
-winit <- runif(31, -1, 1)
-
-error = c()
-for(i in 1:10){
-  # number of hidden neurons (vertices) in each layer: 10
-  # stop the threshold automatically
+# Ridge regression by using n-fold cross-validation
+RidgeRegre = function (X, Y, Lam, Nfolds){
   
-  nn <- neuralnet(Sin ~ Var, data = tr, hidden = 10, threshold = i/1000, 
-                  startweights = winit)
+  N = length(Y)
+  K = ceiling(N/Nfolds)
   
-  # The validation set for early stop of the gradient descent.
-  # fitted values yp by using validation dataset
-  y_v = compute(nn, va$Var)
-  yp = y_v$net.result
-  # compute error function
-  error[i] = mean((va$Sin - yp)^2)
+  #X = scale(X, scale = FALSE)
+  #Y = scale(Y, scale = FALSE)
   
+  # make n subsets of data
+  indxFolds = numeric(N)
+  for (kk in 1:K-1){
+    set.seed(12345)
+    indxFolds[((kk-1)*Nfolds+1):(kk*Nfolds)] = sample(c(1:Nfolds))
+  }
+  set.seed(12345)
+  indxFolds[((K-1)*Nfolds+1):N] = sample(c(1:Nfolds), N-(K-1)*Nfolds)
+  #indxFolds = c(1,2,3,4,5,6,7,8,9,10,10,10,10,10,10,10)
+  CV = 0;
+
+  #model fit for each fold
+  for (ii in 1:Nfolds){
+    
+    valInd = (indxFolds==ii)
+    
+    # divide data into training data and validation data
+    validX = X[valInd,]
+    trainX = X[! valInd,]
+    validY = Y[valInd]
+    trainY = Y[! valInd]
+    
+    trainX1 = scale(trainX, center = TRUE, scale = FALSE)
+    trainY1 = scale(trainY, center = TRUE, scale = FALSE)
+    if (is.null(dim(validX))){
+      validX1 = validX-colMeans(trainX)
+    }
+    else
+    {
+      validX1 = scale((validX),(colMeans(trainX)), FALSE)
+    }
+    
+    validY1 = validY-mean(trainY)
+      
+    # Estimate w
+    I = diag(dim(trainX1)[2])
+    estW = ((solve((t(trainX1) %*% trainX1) + Lam *I)) %*% t(trainX1) )%*%trainY1
+    # Prediction
+    estY = validX1 %*% estW
+    
+    #calculate sum of cross-validation score
+    sqsum = sum((validY1-estY)^2)
+    CV = CV+sqsum
+    
+    
+   valInd = NULL
+   validX = NULL
+   trainX = NULL
+   validY = NULL
+   trainY = NULL
+   validX1 = NULL
+   trainX1 = NULL
+   validY1 = NULL
+   trainY1 = NULL
+  }  
+
+  # Return CV score
+  return(CV/N)
 }
-error
-plot(error, type="l", main = "MSE")
 
+# load data
+data = unname(as.matrix(longley))
+dataX = data[,1:(dim(longley)[2]-1)]
+dataY = matrix(longley$Employed)
 
-# chosen value for the threshold
-index = which.min(error)
-index
-#[1] 4
+# Lambda 
+L = c(1:7)
 
-# final neural network
-plot(nn <- neuralnet(Sin ~ Var, data = tr, hidden = 10, threshold = 4/1000, 
-                     startweights = winit))
+# Number of folds
+Nfolds = 10
+CV = numeric(length(L))
 
-# Plot of the predictions (blue dots) and the data (red dots)
-plot(prediction(nn)$rep1, col="blue")
-points(trva, col = "red")
+for (jj in 1:length(L)){
+  # Calculate cross validation value for each lambda
+  CV[jj] =  RidgeRegre(as.matrix(dataX), dataY, L[jj], Nfolds)
+}
+
+ plot(L,CV, col="red", xlab = "Lambda values", ylab = "CV scores") 
+ 
