03c141fce94f970feaf53b8a9638e00573c32a69
Yumeng Li <lemonisa.lee@gmail.com>
2017-01-03 22:09:39 +0100

    Update ml.R

--- ml.R
+++ ml.R
@@ -1,75 +1,51 @@
+###### Assignment 3
+mydata = read.csv("C:/Users/Sam/Desktop/machine learning/old exam/wine.csv")
+View(mydata)
 
-###### Assignment 2
-attach(mtcars)
-View(mtcars)
-
-# Scale predictors qsec and hp
-mtcars$qsec = scale(mtcars$qsec)
-mtcars$hp = scale(mtcars$hp)
-
-### 2.1 Plot the data in the coordinates hp versus qsec
-# where the data are colored by am.
-index = (mtcars$am==0)
-plot(mtcars$qsec[index],mtcars$hp[index], col = "red", 
-     xlab = "qsec", ylab = "hp")
-points(mtcars$qsec[!index], mtcars$hp[!index], col = "blue")
-
-
-### 2.2 Perform LDA with response am and predictors qsec and hp
-### a) Equal priors
-library(MASS)
-resLDA=lda(am~qsec+hp, data=mtcars, prior=c(.5,.5))
-print(resLDA)
-
-Pred=predict(resLDA)
-plot(mtcars$qsec, mtcars$hp, col=as.double(Pred$class)+1, pch=21, bg=as.double(Pred$class)+1, main="Prediction") 
-
-# Misclassified items
-table(Pred$class, mtcars$am)
-
-### b) Proportional priors
-resLDA=lda(am~qsec+hp, data=mtcars, prior=c(.3,.7))
-
-### 2.3 Implement kernel density estimation with Epanechnikov kernel
-epan_k = function(X, Y, Xtest, lambda){
-  
-  n = length(Xtest)
-  pref = numeric(n)
-  for (i in 1:n){
-    dis = abs(X-Xtest[i])
-    Kl = numeric(length(dis))
-    for (j in 1: length(dis)){
-      if (dis[j]/lambda<1){
-        Kl[j] = (1-(dis[j]^2)/(lambda^2))
-      }else{
-        Kl[j] = 0
-      }
-      
-    }
-    pref[i] = sum(Kl*Y)/sum(Kl)
-  }
-  return(pref)
+### 3.1 change the class "2" to "-1"
+# in order to model a classification problem using the neuralnet package.
+for(i in 1:nrow(mydata)){
+  if(mydata[i, 1] == 2) mydata[i, 1] = -1
 }
+View(mydata)
 
-# a)
-# matrix X
-index = (mtcars$am==0)
-X = matrix(c(mtcars$qsec[index],mtcars$hp[index]), ncol = 2) 
-Xtest = matrix(c(mtcars$qsec,mtcars$hp), ncol = 2) 
-lambda = 0.2
-# Test the function 
-Ytest = epan_k(X, Y=mtcars$am[index], Xtest, lambda)
-plot(mtcars$qsec, mtcars$hp, col = "blue", xlab = "qsec", ylab = "hp")
-points(Xtest, Ytest, pch = 20, col = "red")
+### 3.2 Separate the data set into 70 percent training/ 30 percent test
+set.seed(12345) 
+n=dim(mydata)[1]
+set.seed(12345) 
+id=sample(1:n, floor(n*0.7)) 
+train2=mydata[id,] 
+test2=mydata[-id,] 
 
-# b)
-# matrix X
-index = (mtcars$am==1)
-X = matrix(c(mtcars$qsec[index],mtcars$hp[index]), ncol = 2) 
-Xtest = matrix(c(mtcars$qsec,mtcars$hp), ncol = 2) 
-lambda = 0.2
-# Test the function 
-Ytest = epan_k(X, Y=mtcars$am[index], Xtest, lambda)
-plot(mtcars$qsec, mtcars$hp, col = "blue", xlab = "qsec", ylab = "hp")
-points(Xtest, Ytest, pch = 20, col = "red")
+### 3.3 Fit a neural network
+library(neuralnet)
+set.seed(12345)
+# 0 hidden nodes and tanh as the activation function
+model1 <- neuralnet(class~alcohol+malic_acid+ash+ash_alcalinity+magnesium+total_phenols+flavanoids+nonflavanoid_phenols+proanthocyanins+color_intensity+hue+OD280+proline, data = train2, hidden = 0, act.fct = "tanh")
+model1
+# Estimated Network Weights 
+model1$result.matrix
+# Plot model
+plot(model1)
 
+### 3.4 Compute the misclassification rate of the training and test set
+# plot fitted values
+# train2[,-1]: all cols of features(expect the object:class) 
+pred1 = compute(model1, train2[,-1])$net.result
+# convert 0,1 to -1,1
+pred1 = as.integer(pred1>0)*2-1
+true1 = train2$class
+conf1 = table(true1,pred1)
+misc1 = (1-sum(diag(conf1))/length(true1))
+conf1
+misc1
+
+# test set
+pred2 = compute(model1, test2[,-1])$net.result
+# convert 0,1 to -1,1
+pred2 = as.integer(pred2>0)*2-1
+true2 = test2$class
+conf2 = table(true2,pred2)
+misc2 = (1-sum(diag(conf2))/length(true2))
+conf2
+misc2
