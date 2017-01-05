758bf6dff3ac0f7820de60f90864f7ee073bdcf8
Yumeng Li <lemonisa.lee@gmail.com>
2017-01-05 13:44:30 +0100

    Update ml.R

--- ml.R
+++ ml.R
@@ -1,52 +1,38 @@
-1
-###### Assignment 3
-mydata = read.csv("C:/Users/Sam/Desktop/machine learning/old exam/wine.csv")
-View(mydata)
+#####################################
+# Estimate MSE of the optimal model by using the test set. 
+pcr.fit1 = pcr(Viscosity~., ncomp=40,  data = train, scale = FALSE, validation = "none")  # 40######## 30
+summary(pcr.fit1)
 
-### 3.1 change the class "2" to "-1"
-# in order to model a classification problem using the neuralnet package.
-for(i in 1:nrow(mydata)){
-  if(mydata[i, 1] == 2) mydata[i, 1] = -1
-}
-View(mydata)
+y1 = predict(pcr.fit1, newdata = test)
+mse = mean((y1-test$Viscosity)^2,na.rm= TRUE)
+mse
 
-### 3.2 Separate the data set into 70 percent training/ 30 percent test
+
+### 2.5 Divide the data into training(50%) and test(50%) sets.
+n=dim(data)[1]
 set.seed(12345) 
-n=dim(mydata)[1]
-set.seed(12345) 
-id=sample(1:n, floor(n*0.7)) 
-train2=mydata[id,] 
-test2=mydata[-id,] 
+id=sample(1:n, floor(n*0.5)) 
+train=data[id,] 
+test=data[-id,]
 
-### 3.3 Fit a neural network
-library(neuralnet)
+#### 2.6 use PLS model
+library(pls)
 set.seed(12345)
-# 0 hidden nodes and tanh as the activation function
-model1 <- neuralnet(class~alcohol+malic_acid+ash+ash_alcalinity+magnesium+total_phenols+flavanoids+nonflavanoid_phenols+proanthocyanins+color_intensity+hue+OD280+proline, data = train2, hidden = 0, act.fct = "tanh")
-model1
-# Estimated Network Weights 
-model1$result.matrix
-# Plot model
-plot(model1)
 
-### 3.4 Compute the misclassification rate of the training and test set
-# plot fitted values
-# train2[,-1]: all cols of features(expect the object:class) 
-pred1 = compute(model1, train2[,-1])$net.result
-# convert 0,1 to -1,1
-pred1 = as.integer(pred1>0)*2-1
-true1 = train2$class
-conf1 = table(true1,pred1)
-misc1 = (1-sum(diag(conf1))/length(true1))
-conf1
-misc1
+# Estimate MSE of the optimal model by using the test set.
+pls.fit = plsr(train$Viscosity~., data = train, scale = FALSE, validation = "CV")
+summary(pls.fit)
 
-# test set
-pred2 = compute(model1, test2[,-1])$net.result
-# convert 0,1 to -1,1
-pred2 = as.integer(pred2>0)*2-1
-true2 = test2$class
-conf2 = table(true2,pred2)
-misc2 = (1-sum(diag(conf2))/length(true2))
-conf2
-misc2
+# Plot MSPE
+validationplot(pls.fit, val.type = "MSEP")
+
+# Estimate MSE of the optimal model by using the test set. 
+pls.fit2 = plsr(Viscosity~., ncomp=9,  data = train, scale = FALSE, validation = "none")
+summary(pls.fit2)
+y2 = predict(pls.fit2, newdata = test)
+mse2 = mean((y2-test$Viscosity)^2,na.rm= TRUE)
+mse2
+
+
+
+
