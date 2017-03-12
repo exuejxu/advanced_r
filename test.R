3ab25905ce7c895153812fcc9937a780374f6052
Yumeng Li <lemonisa.lee@gmail.com>
2017-03-12 01:06:28 +0100

    Update test.R

--- test.R
+++ test.R
@@ -1,22 +1,11 @@
-## from ?optmize
-require(graphics)
-     
-f <- function (x, a) (x - a)^2
-xmin <- optimize(f, c(0, 1), tol = 0.0001, a = 1/3)
-print(xmin)
-     
-## See where the function is evaluated:
-optimize(function(x) x^2*(print(x)-1), lower = 0, upper = 10)
-     
-## "wrong" solution with unlucky interval and piecewise constant f():
-f  <- function(x) ifelse(x > -1, ifelse(x < 4, exp(-1/abs(x - 1)), 10), 10)
-fp <- function(x) { print(x); f(x) }
-     
-plot(f, -2,5, ylim = 0:1, col = 2)
-xmin1<-optimize(fp, c(-4, 20))   # doesn't see the minimum
-print("sssssssssssssssssssssssss")
-print(xmin1)
-print("=========================")
-xmin2<-optimize(fp, c(-7, 20))   # ok
-print("sssssssssssssssssssssssss")
-print(xmin2)
+## from ?optim
+## "wild" function , global minimum at about -15.81515
+fw <- function (x)
+         10*sin(0.3*x)*sin(1.3*x^2) + 0.00001*x^4 + 0.2*x+80
+plot(fw, -50, 50, n = 1000, main = "optim() minimising 'wild function'")
+# method = c("Nelder-Mead", "BFGS", "CG", "L-BFGS-B", "SANN","Brent")     
+res <- optim(50, fw, method = "SANN",lower = -Inf, upper = Inf, control = list(maxit = 20000, temp = 20, parscale = 20), hessian = FALSE)
+# res$par= -15.8144, res$value=67.47249, res$$counts["function"]=20000,res$$counts["gradient"]=NA, res$convergence=0 (did!), res$message=NULL
+
+
+
