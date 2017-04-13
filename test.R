439d31885426208d41a86ae37b7ef6b32c25e26b
Yumeng Li <lemonisa.lee@gmail.com>
2017-04-13 17:15:05 +0200

    Update test.R

--- test.R
+++ test.R
@@ -1,115 +1,46 @@
-### Question 1: Genetic algorithm
-### 1.1 Define the function
-f <- function(x) {
-  x^2 / exp(x) - 2*exp((-1) * (9*sin(x)) / (x^2 + x + 1))
-}
+#### Assignment 1. Spam classification with nearest neighbors
 
-### 1.2 Define the function crossover()
-crossover <- function(x, y) {
-  (x + y) / 2
-}
+#### 1.1 Import data
+data <- read.csv("C:/Users/Sam/Desktop/machine learning/lab1/spambase.csv")
 
-### 1.3 Define the function mutate()
-# modulus (x mod y) 5%%2 is 1
-# x %/% y	integer division 5%/%2 is 2
-mutate <- function(x) {
-  x^2 %% 30
-}
+# divide it into training and test sets (50%/50%)
+n=dim(data)[1]
+set.seed(12345)
+id=sample(1:n, floor(n*0.5))
+train=data[id,]
+test=data[-id,]
 
-### 1.4
-myGenetic <- function(maxiter, mutprob){
-  ### (a)(b)(c)
-  # Plot f(x)
-  plot(seq(0,30,0.01), f(seq(0,30,0.01)), type = "l", 
-       xlab = "x", ylab = "f(x)") 
-  # Defines an initial population X
-  X = seq(from = 0, to = 30, by = 5)
-  points(X, f(X), col = "blue", pch = 20)
+traindata = as.matrix(train)
+testdata = as.matrix(test)
+
+#### 1.2 create K-nearest neighbors fun()
+knearest <- function(data, k, newdata){
+  X = data[,-ncol(data)]   #except the last col of spam
+  Y = newdata[,-ncol(data)]
   
-  # Computes vector Values
-  Values = f(X)
-  maxV = c()
-  ### (d) Performs maxiter iterations
-  for(i in 1:maxiter) {
-    parents = sample(X, size=2)
-    victim = which.min(Values)
-    
-    kid = crossover(x=parents[1], y=parents[2])
-    # Mutate this kid with probability mutprob
-    if(runif(1) <= mutprob) kid=mutate(kid)
-    # The victim is replaced by the kid in the population 
-    X[victim] = kid
-    # vector Values is updated.
-    Values[victim] = f(kid)
-    maxV[i] = max(Values)
-  }
-
-  points(X, Values, col = "red", pch=8)
-  legend("topright", c("Original", "Current"), 
-         lty=c(1,1), cex = 0.8,col = c("blue","red"))
+  # compute d(x,y) 
+  Xhat = X/sqrt(rowSums(X^2))
+  Yhat = Y/sqrt(rowSums(Y^2))
   
-  return(list(finalX=X, finalValues=Values, max_value=maxV))
+  # Compute distance 
+  C = Xhat %*% t(Yhat)
+  dist = 1-C 
+  #View(dist) # 1370*1370
+  
+  # order index of the col of dist, 2 for col, increase, each col for each Y 
+  order_dist = apply(dist,2,order) 
+  #View(order_dist) # 1370*1370
+  
+  # Compute predicted class probabilities
+  index_x = order_dist[1:k, ]
+  #View(index_x) # 5*1370
+  
+  nearest = matrix(data[index_x,ncol(data)],nrow = k,ncol=ncol(order_dist)) 
+  #View(nearest) # 5*1370
+  ki = colSums(nearest)
+  print(ki)
+  prob = ki/k
+  return(prob)
 }
 
-myGenetic(10, 0.1)
-myGenetic(10, 0.5)
-myGenetic(10, 0.9)
-myGenetic(100, 0.1)
-myGenetic(100, 0.5)
-myGenetic(100, 0.9)
-
-### Question 2: EM algorithm
-data = read.csv("C:/Users/Sam/Desktop/Computational Statistics/lab6/physical1.csv") 
-
-### 2.1 Make a time series plot
-X = data$X
-Y = data$Y
-Z = data$Z
-
-plot(X, Y, type = "l", col="blue", ylab = "Y / Z")
-lines(X, Z, col="red")
-legend("topright", c("Y", "Z"), 
-       lty=c(1,1), lwd=c(.5,.5),col = c("blue","red"))
-
-### 2.2 2.3  
-#Define log-likelihood function
-ll <- function(X, Y, Zobs, Xobs, lambda, n){
-  2*sum(log(X)) - ( n*log(2) ) - ( 2*n*log(lambda) ) -
-    ( sum(Y*X)/lambda ) - ( sum(Zobs*Xobs)/(2*lambda) )
-}
-
-EM <- function(X,Y,Z){
-  Xobs <- X[!is.na(Z)]
-  Xmiss <- X[is.na(Z)]
-  Zobs <- Z[!is.na(Z)]
-  n <- length(X)
-  m <- length(Xmiss)
-  #Initial values
-  lambdat <- 100
-
-  repeat{
-    #M-step
-    lambdat1 <- ( sum(Y*X) + (sum(Zobs*Xobs)/2) + (lambdat*m) )/(2*n)
-    #Compute log-likelihood using current estmates
-    llt <- ll(X, Y, Zobs, Xobs, lambdat1, n)
-    #Print current parameter values and likelihood
-    cat("lambda =",lambdat1, "log-likelihood =", llt, "\n")
-    #Stop if converged
-    if(abs(lambdat1 - lambdat) < 0.001) break
-    lambdat <- lambdat1
-  }
-}
-EM(data$X, data$Y, data$Z)
-#optimal lambda 10.69566
-
-lambda <- 10.69566
-X <- data$X
-Y <- data$Y
-Z <- data$Z
-EY <- lambda/X
-EZ <- (2*lambda)/X
-plot(X, Z, type="l", ylab="")
-lines(X, EZ, col="red", lwd=2)
-lines(X, Y, col="blue")
-lines(X, EY, col="green", lwd=2)
-legend("topright", c("Y","Z","EY","EZ"), col=c("blue","black","green","red"), lwd=1)
+#knearest(traindata, k=5, testdata)
