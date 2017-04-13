6d56a6d643b11960c40304182063351fd0118031
Yumeng Li <lemonisa.lee@gmail.com>
2017-04-13 17:26:47 +0200

    Update test.R

--- test.R
+++ test.R
@@ -1,46 +1,23 @@
 #### Assignment 1. Spam classification with nearest neighbors
 
-#### 1.1 Import data
-data <- read.csv("C:/Users/Sam/Desktop/machine learning/lab1/spambase.csv")
+#### 1.3 Classify the training and test data by using K=5
+# report the confusion matrix (use table())
+# misclassification rate for training and test data
 
-# divide it into training and test sets (50%/50%)
-n=dim(data)[1]
-set.seed(12345)
-id=sample(1:n, floor(n*0.5))
-train=data[id,]
-test=data[-id,]
+# test data
+fit1 = knearest(traindata, k=5, testdata) # fit:probility %
+predY1 = as.integer(fit1>0.5)   # pred: classify(0,1) as.integer:(TRUE FALSE)->(1,0) 
+true1 = testdata[,ncol(data)]3
+conf1 = table(true1,predY1)
+misc1 = (1-sum(diag(conf1))/length(true1))
+conf1
+misc1
 
-traindata = as.matrix(train)
-testdata = as.matrix(test)
-
-#### 1.2 create K-nearest neighbors fun()
-knearest <- function(data, k, newdata){
-  X = data[,-ncol(data)]   #except the last col of spam
-  Y = newdata[,-ncol(data)]
-  
-  # compute d(x,y) 
-  Xhat = X/sqrt(rowSums(X^2))
-  Yhat = Y/sqrt(rowSums(Y^2))
-  
-  # Compute distance 
-  C = Xhat %*% t(Yhat)
-  dist = 1-C 
-  #View(dist) # 1370*1370
-  
-  # order index of the col of dist, 2 for col, increase, each col for each Y 
-  order_dist = apply(dist,2,order) 
-  #View(order_dist) # 1370*1370
-  
-  # Compute predicted class probabilities
-  index_x = order_dist[1:k, ]
-  #View(index_x) # 5*1370
-  
-  nearest = matrix(data[index_x,ncol(data)],nrow = k,ncol=ncol(order_dist)) 
-  #View(nearest) # 5*1370
-  ki = colSums(nearest)
-  print(ki)
-  prob = ki/k
-  return(prob)
-}
-
-#knearest(traindata, k=5, testdata)
+# training data
+fit2 = knearest(traindata, k=5, traindata)
+predY2 = as.integer(fit2>0.5)
+true2 = traindata[,ncol(data)]
+conf2 = table(true2,predY2)
+misc2 = (1-sum(diag(conf2))/length(true2))
+conf2
+misc2
