e3ebe2573ed2ef6ae233de02d31908c9f5fdc4da
Yumeng Li <lemonisa.lee@gmail.com>
2017-05-20 18:44:14 +0200

    Update test.R

--- test.R
+++ test.R
@@ -1,154 +1,322 @@
-#
-### 1. Linear and polynomial regression
-d = read.table("C:/Users/Sam/Desktop/732A91 Bayesian Learning/Module 2 - Bayesian Regression and Classification/TempLinkoping2016.txt",
-               header = T)
-
-### 1.a Determining the prior distribution of the model parameter.
-# fits a quadratic model using ordinary least squares
-# preliminary exploratory step
-fit = lm(temp ~ time + I(time^2), data=d)
-
-beta_lm = fit$coefficients
-# (Intercept)        time   I(time^2) 
-#   -10.67535    93.59796   -85.83109 
-names(beta_lm) <- NULL
-# [1] -10.67535  93.59796 -85.83109
-
-X = matrix(c(d$time, (d$time)^2), ncol=2)
-X = cbind(1,X)
-Y = matrix(d$temp,ncol = 1)
-n = nrow(d)
-
-mu0 <- beta_lm #lm function result
-omega0 <- diag(length(mu0)) #identity diagonal matrix
-nu0 <- 2
-sigma02 <- 6
-
-### 1.b Check if your prior from a) is sensible.
-# simulate draws from the joint prior
-plot(d$time,d$temp,cex=0.6,pch=16,xlab="Time",ylab="Temp")
 
 library(mvtnorm)
-rbeta = matrix(0,nrow=5000, ncol=length(mu0))
-for(i in 1:5000){
-  # simulate posterior sigma2
-  temp <- rchisq(n=1, df=nu0) #chi-square
-  rsigma2 <- (nu0*sigma02) / temp #inversed chi-square
+library(msm)
+
+### 1.Normal model, mixture of normal model with semi-conjugate prior.
+rain = read.table("C:/Users/Sam/Desktop/732A91 Bayesian Learning/Module 3 - MCMC and Variational Bayes/rainfall.dat",
+                 header = F)
+
+### (1a) Normal model.
+x = rain[,1]
+n = nrow(rain)
+
+# set up the sup-initial parameters
+# prior: mu~N(mu0,tao02)
+mu0 <- 40
+tao02 <- 1e5
+
+# prior: sigma2~Inv-chi^2(nu0,sigma02)
+nu0 <- 3
+sigma02 <- 1000
+
+# initial values
+mu <- 40
+sigma2 <- 2000
+draws[1,1] <- mu     #best guess:mean(x)
+draws[1,2] <- sigma2 #best guess:var(x)
+
+nDraws <- 1000
+draws <- matrix(NA,nDraws,2) 
+
+for(i in 2:nDraws){
   
-  # simulate posterior beta
-  rbeta[i,] = rmvnorm(n=1, mean=mu0, sigma=rsigma2*solve(omega0))
-  
-  yhat = X %*% rbeta[i, ]
-  lines(d$time,yhat,col=rgb(red=0.2, green=0.2, blue=1.0, 
-                            alpha=0.02))
-}
+  # mu~N(mu_n,tao_n2)  
+  tao_n2 = 1 / (n/sigma2+1/tao02)
+  w = n/sigma2 / (n/sigma2+1/tao02)
+  mu_n = w*mean(x) + (1-w)*mu0
 
-beta_hat = colMeans(rbeta)
-# [1] -10.60961  93.55857 -85.69468
+  mu = rnorm(1,mean=mu_n,sd=sqrt(tao_n2))
+  draws[i,1] <- mu
 
+  # sigma2~N(nu_n,sigma_n2)
+  nu_n = nu0 + n
+  sigma_n2 = (nu0*sigma02 + sum((x-mu)^2)) / (nu_n)
 
-### 1.c simulates from the joint posterior distribution
-mu_n = solve(t(X)%*%X + omega0) %*% ((t(X)%*%X)%*%beta_hat + omega0%*%mu0)
-omega_n = t(X)%*%X + omega0
-n = nrow(d)
-nu_n = nu0 + n
-sigma_n2 = (nu0*sigma02 + (t(Y)%*%Y+t(mu0)%*%omega0%*%mu0 - 
-                           t(mu_n)%*%omega_n%*%mu_n)) / nu_n
-
-rbeta_pos = matrix(0,nrow=5000, ncol=length(mu0))
-for(i in 1:5000){
-  # simulate posterior sigma2
   temp <- rchisq(n=1, df=nu_n) #chi-square
-  rsigma2_p <- as.numeric((nu_n*sigma_n2) / temp) #inversed chi-square
+  sigma2 <- (nu_n*sigma_n2) / temp #scaled inversed chi-square
+  draws[i,2] <- sigma2
+}
+
+plot(draws[,1],type="l",xlab = "Iterations",ylab = expression(mu))
+plot(draws[,2],type="l",xlab = "Iterations",ylab = expression(sigma^2))
+
+mean(draws[,1])
+mean(draws[,2])
+ 
+########## (1b) two-component Mixture normal model.
+# Model options
+nComp <- 2    # Number of mixture components
+
+# Prior options
+alpha <- 10*rep(1,nComp) # Dirichlet(alpha)
+# prior of theta/ mu
+muPrior <- rep(0,nComp) # Prior mean of theta
+tau2Prior <- rep(10,nComp) # Prior std theta
+# prior of sigma2
+sigma2_0 <- rep(var(x),nComp) # s20 (best guess of sigma2)
+nu0 <- rep(4,nComp) # degrees of freedom for prior on sigma2
+
+# MCMC options
+nIter <- 1000 # Number of Gibbs sampling draws
+################   END USER INPUT ###############
+
+###### Defining a function that simulates from the 
+rScaledInvChi2 <- function(n, df, scale){
+  return((df*scale)/rchisq(n,df=df))
+}
+
+####### Defining a function that simulates from a Dirichlet distribution
+rDirichlet <- function(param){
+  nCat <- length(param)
+  thetaDraws <- matrix(NA,nCat,1)
+  for (j in 1:nCat){
+    thetaDraws[j] <- rgamma(1,param[j],1)
+  }
+  thetaDraws = thetaDraws/sum(thetaDraws) # Diving every column of ThetaDraws by the sum of the elements in that column.
+  return(thetaDraws)
+}
+
+# Simple function that converts between two different representations of the group allocation
+S2alloc <- function(S){
+  n <- dim(S)[1]
+  alloc <- rep(0,n)
+  for (i in 1:n){
+    alloc[i] <- which(S[i,] == 1)
+  }
+  return(alloc) #length(x), 1,2,2,1,...
+}
+
+# Initial value for the MCMC
+nObs <- length(x)
+S <- t(rmultinom(nObs, size = 1 , prob = rep(1/nComp,nComp))) # nObs-by-nComp matrix with component allocations.
+probObsInComp <- rep(NA, nComp)
+#mu
+theta <- quantile(x, probs = seq(0,1,length = nComp)) 
+#sigma2 
+sigma2 <- rep(var(x),nComp) 
+
+res_theta <- matrix(NA,nIter,nComp) 
+res_sigma2 <- matrix(NA,nIter,nComp) 
+res_theta[1,] <- theta
+res_sigma2[1,] <- sigma2 
+
+res_w <- matrix(1,nIter,nComp) 
+
+for (k in 2:nIter){
+  #message(paste('Iteration number:',k))
+  alloc <- S2alloc(S) #function of convert to group allocation
+  nAlloc <- colSums(S)
+  # Update components probabilities
+  w <- rDirichlet(alpha + nAlloc)
+  res_w[k,] <- w 
   
-  # simulate posterior beta
-  rbeta_pos[i,] = rmvnorm(n=1, mean=mu_n, sigma=rsigma2_p*solve(omega_n))
+  # Update theta's/ mu's
+  for (j in 1:nComp){
+    tau2Post <- 1 / (nAlloc[j]/sigma2[j] + 1/tau2Prior[j])
+    wPrior <- (nAlloc[j]/sigma2[j]) / (nAlloc[j]/sigma2[j] + 1/tau2Prior[j])
+    muPost <- wPrior*mean(x[alloc == j]) + (1-wPrior)*muPrior
+    theta[j] <- rnorm(1, mean = muPost, sd = sqrt(tau2Post))
+  }
+  res_theta[k,] <- theta
+  
+  # Update sigma2's
+  for (j in 1:nComp){
+    df = nu0[j] + nAlloc[j]
+    scale = (nu0[j]*sigma2_0[j] + sum((x[alloc==j]-theta[j])^2)) / df
+    sigma2[j] <- rScaledInvChi2(1,df,scale)
+  }
+  res_sigma2[k,] <- sigma2
+  
+  # Update allocation
+  for (i in 1:nObs){
+    for (j in 1:nComp){
+      probObsInComp[j] <- w[j]*dnorm(x[i], mean = theta[j], sd = sqrt(sigma2[j]))
+    }
+    S[i,] <- t(rmultinom(1, size = 1 , prob = probObsInComp/sum(probObsInComp)))
+  }
 }
-# estimate beta
-beta_hat_pos = colMeans(rbeta_pos)
 
-# posterior credible interval
-# lower 5% and upper 95%
-CI = list()
-for(j in 1:ncol(rbeta_pos)){ #3
-  CI[[j]]=quantile(rbeta_pos[,j], probs = c(0.025, 0.975))
+# mu1: MCMC converge needs 2 plots 
+par(mfrow = c(1,2))
+plot(res_theta[,1], type = "l",xlab = "Iterations",ylab = expression(mu),
+     main = expression(mu[1]))
+
+t1 = res_theta[,1] 
+res1 = c()
+for(i in 1:length(t1)){
+  res1[i] = mean(t1[1:i])
 }
-CI
-#[[1]]
-#5%       95% 
-#  -11.42643  -9.79097  
-#[[2]]
-#5%      95% 
-#  90.32303 96.86747 
-#[[3]]
-#5%       95% 
-#  -88.94770 -82.55349 
-beta_lower <- as.vector(c(CI[[1]][1],CI[[2]][1],CI[[3]][1]))
-beta_upper <- as.vector(c(CI[[1]][2],CI[[2]][2],CI[[3]][2]))
+plot(res1, type = "l",xlab = "Iterations",ylab = expression(E(mu[i])))
 
-plot(d$time,d$temp,cex=0.6,pch=16,xlab="Time",ylab="Temp")
-lines(d$time, y=X%*%beta_hat_pos, col="red")
-lines(d$time, y=X%*%beta_lower, col="blue")
-lines(d$time, y=X%*%beta_upper, col="blue")
+# mu2
+plot(res_theta[,2], type = "l",xlab = "Iterations",ylab = expression(mu),
+     main = expression(mu[2]))
+t2 = res_theta[,2] 
+res2 = c()
+for(i in 1:length(t2)){
+  res2[i] = mean(t2[1:i])
+}
+plot(res2, type = "l",xlab = "Iterations",ylab = expression(E(mu[i])))
+
+# sigma2_1
+par(mfrow = c(1,2))
+plot(res_sigma2[,1], type = "l",xlab = "Iterations",ylab = expression(sigma^2),
+     main = expression(sigma[1]^2 ))
+
+t3 = res_sigma2[,1] 
+res3 = c()
+for(i in 1:length(t3)){
+  res3[i] = mean(t3[1:i])
+}
+plot(res3, type = "l",xlab = "Iterations",ylab = expression(E(sigma[i]^2)))
+
+# sigma2_2
+plot(res_sigma2[,2], type = "l",xlab = "Iterations",ylab = expression(sigma^2),
+     main = expression(sigma[2]^2 ))
+
+t4 = res_sigma2[,2] 
+res4 = c()
+for(i in 1:length(t4)){
+  res4[i] = mean(t4[1:i])
+}
+plot(res4, type = "l",xlab = "Iterations",ylab = expression(E(sigma[i]^2)))
+
+par(mfrow = c(1,1))
+
+# w
+plot(res_w[-1,1], type = "l",xlab = "Iterations",ylab = expression(w),
+     main = expression(w))
 
 
-### 1.d locate the time with the highest expected temp
-# derivative y by x = 0 
-xhat = -beta_hat_pos[2]/ (2*beta_hat_pos[3]) #use estimated beta
-# [1] 0.5458163
-#use all simulations, result is same.
-xhat = mean(-rbeta_pos[,2]/ (2*rbeta_pos[,3])) 
-# [1] 0.5458492
+### (1c) Graphical comparison.
+# plot a histogram or kernel density estimate of the data.
+hist(x, breaks = 50, main = "", freq = FALSE, xlab = "Perceptitation")
 
-### 1.e
+# plot Normal density in (a)
+xSeq <- seq(min(x)-2*sd(x),max(x)+2*sd(x),length = 1000)
+mu_a <- mean(draws[300:100,1])
+sd_a <- sqrt(mean(draws[300:100,2]))
+lines(xSeq, dnorm(xSeq, mean = mu_a, sd = sd_a), type = "l", col = "red")
+
+# plot Mixture of normals density in (b).
+w <- mean(res_w[300:1000,1])
+mu_b <- colMeans(res_theta[300:1000,])        #[1]69.78689 2033.14458
+sd_b <- sqrt(colMeans(res_sigma2[300:1000,])) #[1]8.353855 45.090405
+
+mixDens <- w*dnorm(xSeq,mean=mu_b[1],sd=sd_b[1]) + 
+  (1-w)*dnorm(xSeq,mean=mu_b[2],sd=sd_b[2])
+
+lines(x = xSeq, mixDens, type = "l", lty = 1, col = "green")
+
+legend("topright", bty = "n",
+       legend = c("Data histogram","Normal density","Mixture density"), 
+       col=c("black","red","green"), lwd=1, cex = 0.8)
 
 
-### 2.Posterior approximation for classification with logistic regression
+
+########################
+### 2. Probit regression
 dat = read.table("C:/Users/Sam/Desktop/732A91 Bayesian Learning/Module 2 - Bayesian Regression and Classification/WomenWork.dat",
-               header = T)
+                 header = T)
 
 y = as.vector(dat$Work)
-x = as.matrix(dat[,-1]) #already have!! 1st col: constant 
+x = as.matrix(dat[,-1])#already have!! 1st col: constant 
 #### if not: x = cbind(1, x)
+n = nrow(dat)
 
-### 2.a & b logistic regression
-# added a zero in the model so that R doesn’t add an extra intercept
-# (we already have an intercept term from the Constant feature).
-glmModel <- glm(Work ~ 0 + ., data = dat, family = binomial)
+### 2(a) Gibbs sampler for the probit regression
+Gibbs_pr <- function(muB,sigmaB,nIter=1000){
+  
+  # initial value
+  u <- as.vector(rep(0,nrow(dat)))
+  
+  res_beta <- matrix(NA,nIter,ncol(x))
+  res_u <- matrix(NA,nIter,nrow(dat))
+  
+  for(i in 1:nIter){
+    
+    # update beta
+    sigmaB_hat <- solve(t(x)%*%x + solve(sigmaB))
+    muB_hat <- sigmaB_hat %*% (t(x)%*%u + solve(sigmaB)%*%muB) ### u
+ 
+    #library(mvtnorm)
+    beta <- rmvnorm(n=1,mean=muB_hat,sigma=sigmaB_hat)
+    res_beta[i,] <- beta
+    
+    # update u
+    #library(msm)
+    u <- rtnorm(nrow(x), mean=x%*%t(beta), lower=ifelse(y>0,0,-Inf), 
+                upper=ifelse(y>0,Inf,0))
+  }
+  colnames(res_beta) <- colnames(x)
+  return(list(u=res_u,beta=res_beta))
+}
 
-# Prior: scaling factor such that Prior Covariance = (tau^2)*I
+
+### 2(b) compute posterior of beta 
+# using prior N(0,(tau^2)*I), tao=10
 tau <- 10
 covNames <- names(dat)[2:length(names(dat))]
 nPara <- ncol(x) #8
 
-# Setting up the prior
+# input prior
+muB <- as.vector(rep(0,nPara)) #mean vector
+sigmaB <- tau^2*diag(nPara)
+
+res <- Gibbs_pr(muB,sigmaB) #call function
+
+betaMeans <- colMeans(res$beta)
+betaSd <- apply(res$beta, 2, sd)
+
+par(mfrow = c(3,3))
+for(i in 1:8){
+  CI <- quantile(res$beta[,i], probs = c(0.025, 0.975))
+  hist(res$beta[,i],breaks = 50,main = colnames(res$beta)[i],
+       xlab = expression(beta),freq = FALSE)
+  abline(v = betaMeans[i], col = "red")
+  abline(v = CI, col = "blue", lty = 2)
+  legend("topleft", legend = c(expression(paste("E[",Beta,"]")),"95% Equal tail CI"),
+         col = c("red","blue"), lty = 1, bty = "n",cex = 0.6)
+}
+par(mfrow = c(1,1))
+
+### 2(c) normal approximation
+covNames <- names(dat)[2:length(names(dat))]
+nPara <- ncol(x)
+
+# prior
 mu <- as.vector(rep(0,nPara)) # Prior mean vector
 Sigma <- tau^2*diag(nPara)
 
-# Defining the functions that returns the log posterior (Logistic models). 
-# 1ST input argument of this function must be the one that we optimize on,
-# i.e. the regression coefficients.
-
-LogPostLogistic <- function(betaVect,y,X,mu,Sigma){
+# Log Posterior
+LogPostProbit <- function(betaVect,y,X,mu,Sigma){
   
-  nPara <- length(betaVect);
-  linPred <- X%*%betaVect;
+  nPara <- length(betaVect)
+  linPred <- X%*%betaVect
   
-  # evaluating the log-likelihood                                    
-  logLik <- sum( linPred*y -log(1 + exp(linPred)))
-  if (abs(logLik) == Inf) logLik = -20000 
-  # Likelihood is not finite, stear the optimizer away from here!
+  # log-likelihood of Probit Regression                                    
+  logLik <- sum(y*pnorm(linPred, log.p=T, lower.tail=T) +     #lower.tail=T: P[X ≤ x] 
+                  (1-y)*pnorm(linPred,log.p=T, lower.tail=F)) #lower.tail=F: P[X > x]
   
-  # evaluating the prior, library(mvtnorm)
+  # prior, library(mvtnorm)
   logPrior <- dmvnorm(betaVect, matrix(0,nPara,1), Sigma, log=TRUE)
   
   # add the log prior and log-likelihood together to get log posterior
   return(logLik + logPrior)
 }
 
-initVal <- as.vector(rep(0,ncol(x))); 
-OptimResults<-optim(initVal,LogPostLogistic,gr=NULL,y,x,mu,Sigma,method=c("BFGS"),control=list(fnscale=-1),hessian=TRUE)
-OptimResults
+initVal <- as.vector(rep(0,ncol(x)))
+OptimResults<-optim(initVal,LogPostProbit,gr=NULL,y,x,mu,Sigma,method=c("BFGS"),control=list(fnscale=-1),hessian=TRUE)
 
 # Printing the results to the screen
 postMode <- OptimResults$par
@@ -157,46 +325,25 @@ names(postMode) <- covNames # Naming the coefficient by covariates
 approxPostStd <- sqrt(diag(postCov)) # Computing approximate standard deviations.
 names(approxPostStd) <- covNames # Naming the coefficient by covariates
 
-# approximate 95% credible interval 
-# for the variable NSmallChild.
-sd <- sqrt(diag(postCov)[which(covNames == "NSmallChild")])
-CI <- c(postMode["NSmallChild"]-1.96*sd, 
-        postMode["NSmallChild"]+1.96*sd)
-#-2.121425, -0.5968414 
 
-# plot CI
-seqc <- seq(postMode["NSmallChild"] - 4*sd,
-            postMode["NSmallChild"] + 4*sd,
-            length = 1000)
-plot(seqc,dnorm(x = seqc,mean = postMode["NSmallChild"],sd = sd),type = "l",
-     main = "NSmallChild",xlab = expression(beta[NSmallChild]),ylab="")
-abline(v = CI,lty = 2,col = "red")
-
-
-### 2.c simulates from the predictive distribution of the response variable in a logistic regression. 
-x_test = matrix(c(1,10,8,10,(10/10)^2,40,1,1),nrow = 1)
-y_pred = c()
-prob = c()
-for(i in 1:1000){
-  # Use your normal approximation to simulate beta vector
-  # library(mvtnorm)
-  sam_beta = as.vector(rmvnorm(1,mean=OptimResults$par, #as.vector()
-                               sigma = -solve(OptimResults$hessian)))
-    
-  temp = exp(x_test%*%sam_beta) # should be a number!!!
-  prob[i] = temp / (1+temp) 
-  y_pred[i] = rbinom(n = 1, size = 1, prob = prob[i])
+# Compare with the results from 2(b).
+par(mfrow = c(3,3))
+for(i in 1:length(postMode)){
+  # normal approximation
+  sd_i <- approxPostStd[i]
+  betaSeq <- seq(postMode[i] - 4*sd_i,
+                 postMode[i] + 4*sd_i,
+                 length = 1000)
+  plot(betaSeq,dnorm(x = betaSeq,mean = postMode[i],sd = sd_i),
+       type = "l", main = names(postMode)[i],col="blue",
+       ylab = "",xlab = expression(beta))
+  
+  # gibbs
+  gibbsDensity <- density(res$beta[,i])
+  lines(x = gibbsDensity$x, y = gibbsDensity$y , col = "red")
+  
+  legend("topleft",legend = c("Normal Approx","Gibbs"),
+         col = c("blue", "red"),lty = c(1,1),bty = "n",cex = 0.6)
 }
+par(mfrow = c(1,1))
 
-mean(prob)
-# predictive distribution for the Work
-hist(y_pred,breaks=30,xlab="Work",probability = TRUE,
-     main="Predictive distribution for the Work variable")
-
-################# same result
-#> x[1:2,]%*%t(b)
-#[,1] -0.2279402
-#[2,] -0.6140388
-#> x[1:2,]%*%as.vector(b)
-#[1,] -0.2279402
-#[2,] -0.6140388
