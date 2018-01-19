5a749cc942c56d736330b7a6dee07622a2ddbe06
Yumeng Li <lemonisa.lee@gmail.com>
2018-01-19 16:12:29 +0100

    Update test.R

--- test.R
+++ test.R
@@ -8,8 +8,24 @@ library(diversitree)
 set.seed(123)
 pars = c(0.1, 0.1, 0, 0, 0.1, 0.1)
 phy <- tree.bisse(pars,max.taxa=50, x0=NA)
-
 plot(history.from.sim.discrete(phy, 0:1), phy, 
      show.tip.label=FALSE,
      main="True history")
 axisPhylo()
+                 
+lik <- make.bisse(phy, phy$tip.state)
+p = starting.point.bisse(phy)
+fit = find.mle(lik, p)
+coef(fit_bisse)
+fit_bisse$lnLik
+constant_lik = constrain(bisse_lik, lambda0 ~ lambda1, mu0 ~0,  mu1~0)                 
+fit_constant = find.mle(constant_lik, p)
+coef(fit_constant)
+fit_constant$lnLik
+ 
+  # tree filter
+freq <- as.vector(table(phy$tip.state))
+if((length(freq) == 1)){ 
+if(any(freq/sum(freq) < 2/50) | (length(freq) == 1)){
+  return(NA)
+}                 
